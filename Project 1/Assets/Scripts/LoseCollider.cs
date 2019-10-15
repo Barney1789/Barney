a59e65584697ac07a19cd77a32b6613b6e58e334
Barney1789 <Barney1789@users.noreply.github.com>
2019-10-15 09:50:03 +0200

    Paddle magic

--- Project 1/Assets/Scripts/LoseCollider.cs
+++ Project 1/Assets/Scripts/LoseCollider.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoseCollider : MonoBehaviour
 {
@@ -8,5 +9,6 @@ public class LoseCollider : MonoBehaviour
     {
         print("Game Over");
         print(myCollider.gameObject.name);
+        SceneManager.LoadScene("death");
     }
 }
---
+++ Project 1/Assets/Scripts/Paddle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenwidthInUnits = 16f;
+    Vector2 paddlePosition;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddlePosition = new Vector2(transform.position.x,transform.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePos = (Input.mousePosition.x / Screen.width * screenwidthInUnits);
+
+       //paddlePosition = new Vector2(mousePos, transform.position.y);
+
+       paddlePosition.x = mousePos;
+       
+        transform.position = paddlePosition;
+    }
+}
