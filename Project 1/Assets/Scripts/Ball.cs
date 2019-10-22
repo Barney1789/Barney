3cc133da2199eba3885f0c27f1b618d10b0dfa69
Barney1789 <Barney1789@users.noreply.github.com>
2019-10-22 09:52:04 +0200

    Block

--- Project 1/Assets/Scripts/Ball.cs
+++ Project 1/Assets/Scripts/Ball.cs
@@ -8,6 +8,8 @@ public class Ball : MonoBehaviour
 
     Vector2 paddleToBallDistance;
 
+    bool hasStarted = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +19,25 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+        }
+        //this.transform.position = myPaddle.transform.position + paddleToBallDistance;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 15f);
+
+        }
+
+    }
+
+
+    private void LockBallToPaddle()
+    {
         Vector2 paddlePosition = myPaddle.transform.position;
         this.transform.position = paddlePosition + paddleToBallDistance;
-        //this.transform.position = myPaddle.transform.position + paddleToBallDistance;
     }
 }
